980ff0aff4321772840d1e7e3ba84d928ac658e6
AshishSah10 <41590024+AshishSah10@users.noreply.github.com>
2020-10-21 21:15:43 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [paraId, setParaId] = useState(false);
+  const handleClick = () => {
+    setParaId("para");
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>
+        Click Me!
+      </button>
+      {paraId === "para" ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
-
 
 export default App;
